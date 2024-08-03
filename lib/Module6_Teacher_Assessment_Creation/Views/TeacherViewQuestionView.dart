05121a4e603fc2a08c63baf422e6cf57308160ef
samuel <sam.angus@symph.co>
2024-08-03 19:33:02 +0800

    Add delete question on question view screen

--- lib/Module6_Teacher_Assessment_Creation/Views/TeacherViewQuestionView.dart
+++ lib/Module6_Teacher_Assessment_Creation/Views/TeacherViewQuestionView.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:team_adaptive/Components/TemplateView.dart';
@@ -15,52 +14,68 @@ class TeacherViewQuestionView extends StatelessWidget {
   Widget questionListView(List<QuestionModel> questionList, bool canEdit) {
     return Consumer<TeacherQuestionViewModel>(
         builder: (context, viewModel, child) {
-          return SingleChildScrollView(
-            child: Wrap(
-              spacing: 10,
-              children: List.generate(
-                  questionList.length,
-                      (index) {
-                    QuestionModel question = questionList[index];
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+      return SingleChildScrollView(
+        child: Wrap(
+          spacing: 10,
+          children: List.generate(questionList.length, (index) {
+            QuestionModel question = questionList[index];
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      question.question,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    const SizedBox(height: 8),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          choiceOption(question.correctAnswer, Colors.green),
+                          ...question.wrongChoices.map((choice) {
+                            return choiceOption(choice, Colors.red);
+                          }),
+                        ]),
+                    if (canEdit)
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Row(
                           children: [
-                            Text(
-                              question.question,
-                              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            TeacherAddQuestionView(
+                                              lessonModel: lesson,
+                                              question: question,
+                                            )));
+                              },
+                              child: const Text('Edit'),
                             ),
-                            const SizedBox(height: 8),
-                            Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  choiceOption(question.correctAnswer, Colors.green),
-                                  ...question.wrongChoices.map((choice) {
-                                    return choiceOption(choice, Colors.red);
-                                  }),
-                                ]
-                            ),
-                            if (canEdit) Align(
-                              alignment: Alignment.centerRight,
-                              child: TextButton(
-                                onPressed: () {
-                                  Navigator.push(context, MaterialPageRoute(builder: (context) => TeacherAddQuestionView(lessonModel: lesson, question: question,)));
-                                },
-                                child: const Text('Edit'),
-                              ),
-                            ),
+                            TextButton(
+                              onPressed: () {
+                                viewModel.deleteQuestion(question);
+                              },
+                              child: const Text('Delete'),
+                            )
                           ],
                         ),
                       ),
-                    );
-                  }),
-            ),
-          );
-        }
-    );
+                  ],
+                ),
+              ),
+            );
+          }),
+        ),
+      );
+    });
   }
+
   Widget choiceOption(String text, Color color) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 2.0),
@@ -85,55 +100,53 @@ class TeacherViewQuestionView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TeacherQuestionViewModel viewModel = Provider.of<TeacherQuestionViewModel>(context, listen: false);
-    return TemplateView(highlighted: SELECTED.NONE, topRight: userInfo(context), child: FutureBuilder<void>(
-        future: viewModel.initializeViewModel(lesson.id!),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            return DefaultTabController(
-                length: 2,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => TeacherAddQuestionView(lessonModel: lesson)));
-                          },
-                          child: const Text('Add Question')
-                      ),
-                      const TabBar(
-                          tabs: [
+    final TeacherQuestionViewModel viewModel =
+        Provider.of<TeacherQuestionViewModel>(context, listen: false);
+    return TemplateView(
+        highlighted: SELECTED.NONE,
+        topRight: userInfo(context),
+        child: FutureBuilder<void>(
+            future: viewModel.initializeViewModel(lesson.id!),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return DefaultTabController(
+                    length: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            TeacherAddQuestionView(
+                                                lessonModel: lesson)));
+                              },
+                              child: const Text('Add Question')),
+                          const TabBar(tabs: [
                             Tab(
                               text: "My Questions",
                             ),
                             Tab(
                               text: "Other Questions",
                             )
-                          ]
+                          ]),
+                          Expanded(
+                              child: TabBarView(children: [
+                            questionListView(viewModel.myQuestionPool, true),
+                            questionListView(
+                                viewModel.otherQuestionPool, false),
+                          ]))
+                        ],
                       ),
-                      Expanded(
-                          child: TabBarView(
-                              children: [
-                                questionListView(viewModel.myQuestionPool, true),
-                                questionListView(viewModel.otherQuestionPool, false),
-                              ]
-                          )
-                      )
-                    ],
-                  ),
-                )
-            );
-          }
-        }
-    ));
+                    ));
+              }
+            }));
   }
 }
-
-
-
-
