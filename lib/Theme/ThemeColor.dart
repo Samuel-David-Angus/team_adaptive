a59c0d8e0a78d536cf06b8b989817f7bdd226b5e
Jjtabanag <jjtabanag.com>
2024-08-11 17:20:47 +0800

    Changed most of the ui

---
+++ lib/Theme/ThemeColor.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  static const Color darkgreyTheme = Color.fromARGB(255, 58, 58, 58);
+  static const Color lightgreyTheme = Color.fromARGB(255, 208, 208, 208);
+  static const Color offwhiteTheme = Color.fromARGB(255, 254, 246, 255);
+  static const Color blueTheme = Color.fromARGB(255, 33, 59, 208);
+}
