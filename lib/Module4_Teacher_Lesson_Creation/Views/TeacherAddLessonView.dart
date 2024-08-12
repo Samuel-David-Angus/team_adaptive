35f8b4d26dbc2fb59cbed05f6e0d996019667065
Jjtabanag <jjtabanag.com>
2024-08-13 03:38:18 +0800

    new add lesson view

--- lib/Module4_Teacher_Lesson_Creation/Views/TeacherAddLessonView.dart
+++ lib/Module4_Teacher_Lesson_Creation/Views/TeacherAddLessonView.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:team_adaptive/Module4_Teacher_Lesson_Creation/View_Models/TeacherLessonViewModel.dart';
 import 'package:team_adaptive/Module4_Teacher_Lesson_Creation/Views/TeacherSelectConceptsView.dart';
+import 'package:team_adaptive/Theme/ThemeColor.dart';
 
 import '../../Module2_Courses/Models/CourseModel.dart';
 import '../View_Models/SelectConceptsViewModel.dart';
@@ -24,75 +25,85 @@ class TeacherAddLessonView extends StatelessWidget {
       content: Padding(
         padding: const EdgeInsets.all(16),
         child: SizedBox(
-          width: MediaQuery.of(context).size.width / 2 - 20,
+          width: MediaQuery.of(context).size.width / 3 - 20,
+          height: MediaQuery.of(context).size.height / 2 - 20,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              const Text('Add Lesson'),
+              const SizedBox(height: 20.0),
               TextField(
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(), hintText: 'Title'),
                 controller: titleController,
               ),
+              const SizedBox(height: 30.0),
               TextField(
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(), hintText: 'Description'),
                 controller: descriptionController,
+                maxLines: 10,
               ),
-              ElevatedButton(
-                  onPressed: () async {
-                    selectedItems = await showDialog<List<String>>(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return ChangeNotifierProvider.value(
-                          value: selectConceptsViewModel,
-                          child: TeacherSelectConceptsView(
-                            courseID: course.id,
-                          ),
-                        );
-                      },
-                    );
-                  },
-                  child: const Text('Concepts')),
-              const SizedBox(
-                height: 20,
-              ),
-              TextButton(
-                  onPressed: () async {
-                    if (selectedItems != null) {
-                      if (selectedItems!.isNotEmpty &&
-                          titleController.text.isNotEmpty &&
-                          descriptionController.text.isNotEmpty) {
-                        await viewModel.addLesson(
-                            titleController.text,
-                            descriptionController.text,
-                            course.id!,
-                            selectedItems!);
-                        Navigator.of(context).pop();
-                      } else {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text('Message'),
-                              content:
-                                  Text('Pls fill all fields and select concepts'),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: Text('OK'),
-                                  onPressed: () {
-                                    Navigator.of(context)
-                                        .pop(); // Close the dialog
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        );
+              const SizedBox(height: 50.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      selectedItems = await showDialog<List<String>>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return ChangeNotifierProvider.value(
+                            value: selectConceptsViewModel,
+                            child: TeacherSelectConceptsView(
+                              courseID: course.id,
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: const Text('Concepts')),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (selectedItems != null) {
+                        if (selectedItems!.isNotEmpty &&
+                            titleController.text.isNotEmpty &&
+                            descriptionController.text.isNotEmpty) {
+                          await viewModel.addLesson(
+                              titleController.text,
+                              descriptionController.text,
+                              course.id!,
+                              selectedItems!);
+                          Navigator.of(context).pop();
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Message'),
+                                content:
+                                    const Text('Pls fill all fields and select concepts'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('OK'),
+                                    onPressed: () {
+                                      Navigator.of(context)
+                                          .pop(); // Close the dialog
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
                       }
-                    }
-                  },
-                  child: Text('Save'))
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: ThemeColor.darkgreyTheme,// Padding
+                    ),
+                    child: const Text('Save', style: TextStyle(color: ThemeColor.offwhiteTheme)),
+                )],
+              ),
+              
             ],
           ),
         ),
