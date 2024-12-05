8adc8ccb3624a96b2c304dac26e7b4f2b243f86f
Jjtabanag <jjtabanag.com>
2024-12-05 21:11:15 +0800

    Removed the default homepage to appear when user refreshes homepage when logged in

--- lib/LandingNavPages/HomePage.dart
+++ lib/LandingNavPages/HomePage.dart
@@ -10,75 +10,93 @@ import '../Module1_User_Management/Services/AuthServices.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Future<String?> _checkAuthStatus(BuildContext context) async {
+    final authServices = Provider.of<AuthServices>(context, listen: false);
+    await authServices.checkAuthStatus(); // Assuming this method checks and updates the auth status
+    return authServices.userInfo?.type;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final authServices = Provider.of<AuthServices>(context);
-    String? type = authServices.userInfo?.type;
-    if (authServices.currentUser == null || type == null) {
-      return SingleChildScrollView(
-        child: Stack(
-          children: [
-            // Background Image
-            Container(
-              height: MediaQuery.of(context).size.height - kToolbarHeight,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/hero-home.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 100.0, vertical: 200.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return FutureBuilder<String?>(
+      future: _checkAuthStatus(context),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SizedBox(); // Loading state
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          final String? type = snapshot.data;
+          if (type == null) {
+            return SingleChildScrollView(
+              child: Stack(
                 children: [
-                  const Text(
-                    'Experience \nAdaptive Learning',
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 60),
-                  const Text(
-                    'AdaptiveEdu is a platform that empowers teachers to create and deliver '
-                    'personalized\nlearning experiences tailored to the needs of each student. ',
-                    style: TextStyle(
-                      color: ThemeColor.darkgreyTheme,
-                      fontSize: 16,
-                      height: 1.5,
-                    ),
-                  ),
-                  const SizedBox(height: 50),
-                  ElevatedButton(
-                    onPressed: () {
-                      GoRouter.of(context).go('/register');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 24, vertical: 16),
-                      backgroundColor: ThemeColor.darkgreyTheme,
-                    ),
-                    child: const Text(
-                      'Sign up now',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: ThemeColor.lightgreyTheme,
+                  // Background Image
+                  Container(
+                    height: MediaQuery.of(context).size.height - kToolbarHeight,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/hero-home.png'),
+                        fit: BoxFit.cover,
                       ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 100.0, vertical: 200.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Experience \nAdaptive Learning',
+                          style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 60),
+                        const Text(
+                          'AdaptiveEdu is a platform that empowers teachers to create and deliver '
+                          'personalized\nlearning experiences tailored to the needs of each student. ',
+                          style: TextStyle(
+                            color: ThemeColor.darkgreyTheme,
+                            fontSize: 16,
+                            height: 1.5,
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        ElevatedButton(
+                          onPressed: () {
+                            GoRouter.of(context).go('/register');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 16),
+                            backgroundColor: ThemeColor.darkgreyTheme,
+                          ),
+                          child: const Text(
+                            'Sign up now',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: ThemeColor.lightgreyTheme,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
-      );
-    }
-    return type == 'teacher'
-        ? const TeacherHomePage()
-        : const StudentHomePage();
+            );
+          } else {
+            return type == 'teacher'
+                  ? const TeacherHomePage()
+                  : const StudentHomePage();
+          }
+        }
+        
+      }
+    );
   }
 }
