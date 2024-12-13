15b1ac762c191de01187b7775e29e8d00c9013e8
Jjtabanag <jjtabanag.com>
2024-12-13 19:34:51 +0800

    Updated TeachHomePage UI

--- lib/HomePages/TeacherHomePage.dart
+++ lib/HomePages/TeacherHomePage.dart
@@ -67,31 +67,81 @@ class TeacherHomePage extends StatelessWidget {
                   style: ElevatedButton.styleFrom(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 24, vertical: 16),
-                    backgroundColor: ThemeColor.offwhiteTheme,
+                    backgroundColor: ThemeColor.darkgreyTheme,
                   ),
                   child: const Text(
                     'Get Started',
                     style: TextStyle(
                       fontSize: 18,
-                      color: ThemeColor.darkgreyTheme,
+                      color: ThemeColor.offwhiteTheme,
                     ),
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    GoRouter.of(context).go('/student-performance');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 24, vertical: 16),
-                    backgroundColor: ThemeColor.offwhiteTheme,
-                  ),
-                  child: const Text(
-                    'Student Performance',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: ThemeColor.darkgreyTheme,
+                const SizedBox(height: 60),
+                Container(
+                  width: MediaQuery.of(context).size.width / 2.5,
+                  decoration: BoxDecoration(
+                    color: ThemeColor.offwhiteTheme,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 6,
+                        blurRadius: 6,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                    border: Border.all(
+                      color: ThemeColor.lightgreyTheme,
+                      width: 2,
                     ),
+                  ),
+                  child: Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Text(
+                          "Start tracking your student's performance",
+                          style: TextStyle(
+                            color: ThemeColor.darkgreyTheme,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                          width: MediaQuery.of(context).size.width / 8),
+                      TextButton(
+                        onPressed: () async {
+                          GoRouter.of(context).go('/student-performance');
+                        },
+                        style: ButtonStyle(
+                          overlayColor:
+                              WidgetStateProperty.resolveWith<Color>(
+                            (Set<WidgetState> states) {
+                              if (states.contains(WidgetState.hovered)) {
+                                return Colors.transparent;
+                              }
+                              return Colors.transparent;
+                            },
+                          ),
+                          textStyle:
+                            WidgetStateProperty.resolveWith<TextStyle>(
+                              (Set<WidgetState> states) {
+                                if (states.contains(WidgetState.hovered)) {
+                                  return const TextStyle(decoration: TextDecoration.underline);
+                                }
+                                return const TextStyle();
+                              }
+                            )
+                        ),
+                        child: const Text(
+                          'Check Now →',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: ThemeColor.darkgreyTheme,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
